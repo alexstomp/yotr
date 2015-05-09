4604123be53c0925d7a796310f8d078b30a240a8
Alex Stomp <stompy1208@gmail.com>
2015-05-08 22:33:36 -0500

    adds basic navigation and styles for the website

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,14 @@
 class ApplicationController < ActionController::Base
-  # Prevent CSRF attacks by raising an exception.
-  # For APIs, you may want to use :null_session instead.
-  protect_from_forgery with: :exception
+    # Prevent CSRF attacks by raising an exception.
+    # For APIs, you may want to use :null_session instead.
+    protect_from_forgery with: :exception
 
-  def home
-  end
+    def home
+    end
+
+    def about
+    end
+
+    def new_post
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
   root 'application#home'
+
+  get 'about' => 'application#about'
+  get 'post' => 'application#new_post', :as => "new_post"
 end
